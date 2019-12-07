25296bd65911c67e47e36acbea71dae53dbd48b3
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 16:48:24 +0530

    Added: Dictionary Wage

--- empWage.sh
+++ empWage.sh
@@ -2,7 +2,10 @@
 
 echo "Welcome to Employee Wage Computation "
 
-randomTime=$((RANDOM%2))
+declare -A dailyWageDictionary
+declare -A totalWageDictionary
+
+
 
 FULL_DAY_WORKING_HOUR=8
 HALF_DAY_WORKING_HOUR=4
@@ -12,35 +15,42 @@ MAX_HRS_IN_MONTH=100
 
 totalEmpHrs=0
 days=0
+totalWage=0
 
 function getWorkingHour(){
+	randomTime=$((RANDOM%2))
 	case $randomTime in 
-	1) workingHour=FULL_DAY_WORKING_HOUR;;
-	0) workingHour=HALF_DAY_WORKING_HOUR;;
+	1) workingHour=$FULL_DAY_WORKING_HOUR;;
+	0) workingHour=$HALF_DAY_WORKING_HOUR;;
 	esac
 	echo $workingHour
 }
 
-for (( days=0;days<NUM_WORKING_DAYS;days++ ))
-do
-	while [[ $days -lt $NUM_WORKING_DAYS && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+while [ $NUM_WORKING_DAYS -gt 0 ]
 	do
 		randomPresent=$((RANDOM%2))
 		if [ $randomPresent -eq 0 ]
 		then
 			echo "Employee is Absent"
 		else
-			workingHour=$( getWorkingHour )
-			dailyWage=$(($workingHour*$WAGE_PER_HOUR))
-			echo "Daily Wage of Employee :- " $dailyWage
+			workingHour=$(getWorkingHour)
 		fi
-		days=$(($days+1))
+      dailyWage=$(($workingHour*$WAGE_PER_HOUR))
+		echo "Daily Wage of Employee :- " $dailyWage
+	#	dailyWage[$days]=$dailyWage
 		totalEmpHrs=$(($totalEmpHrs+$workingHour))
-		dailyWage[$days]=$dailyWage
-		totalWage=$(($totalWage+$dailyWage))
-		totalWages[$days]=$totalWage
-	done
+      totalWage=$(($totalWage+$dailyWage))
+		#totalWages[$days]=$totalWage
+		dailyWageDictionary["Day_$days"]="$dailyWage   $totalWage"
+		#totalWageDictionary["Day $days"]=$totalWage
+		days=$(($days+1))
+		((NUM_WORKING_DAYS--))
 done
-echo ${dailyWage[@]}
-echo ${totalWages[@]}
-echo ${totalWage}
+#echo ${dailyWage[@]}
+
+for i in  "${!dailyWageDictionary[@]}"
+do
+	echo "Day_$i     ${dailyWageDictionary[$i]}"
+	#echo ${totalWageDictionary[@]}
+done
+
