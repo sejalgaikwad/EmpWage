9b888292ec59d0614c6149a0ee2122683b6a0602
sejalgaikwad <sejalgaikwad24@gmail.com>
2019-12-07 13:17:39 +0530

    Added: Condition for Working hours

--- empWage.sh
+++ empWage.sh
@@ -8,6 +8,10 @@ FULL_DAY_WORKING_HOUR=8
 HALF_DAY_WORKING_HOUR=4
 WAGE_PER_HOUR=20
 NUM_WORKING_DAYS=20
+MAX_HRS_IN_MONTH=100
+
+totalEmpHrs=0
+days=0
 
 function getWorkingHour(){
 	case $randomTime in 
@@ -17,18 +21,21 @@ function getWorkingHour(){
 	echo $workingHour
 }
 
-for (( i=0;i<NUM_WORKING_DAYS;i++ ))
+for (( days=0;days<NUM_WORKING_DAYS;days++ ))
 do
-	randomPresent=$((RANDOM%2))
-
-	if [ $randomPresent -eq 0 ]
-	then
-		echo "Employee is Absent"
-	else
-		workingHour=$( getWorkingHour )
-		dailyWage=$(($workingHour*$WAGE_PER_HOUR))
-	   monthlyWage=$(($NUM_WORKING_DAYS*dailyWage))
-		echo "Daily Wage of Employee :- " $dailyWage
-fi
+	while [[ $days -lt $NUM_WORKING_DAYS && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	do
+		randomPresent=$((RANDOM%2))
+		if [ $randomPresent -eq 0 ]
+		then
+			echo "Employee is Absent"
+		else
+			workingHour=$( getWorkingHour )
+			dailyWage=$(($workingHour*$WAGE_PER_HOUR))
+			echo "Daily Wage of Employee :- " $dailyWage
+		fi
+		days=$(($days+1))
+		totalEmpHrs=$(( $totalEmpHrs + $workingHour ))
+	done
 done
 
